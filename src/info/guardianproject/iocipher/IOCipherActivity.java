ca98220c5a4677a8be1cb60d7e35951883676c15
Hans-Christoph Steiner <hans@eds.org>
2012-03-20 14:38:00 -0400

    added skeleton Android project

---
+++ src/info/guardianproject/iocipher/IOCipherActivity.java
@@ -0,0 +1,13 @@
+package info.guardianproject.iocipher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IOCipherActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
