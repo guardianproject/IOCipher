d7e734aec7430ead729aa0da8bedf5780daaed33
Hans-Christoph Steiner <hans@eds.org>
2012-03-28 17:27:29 -0400

    first stab at a File.listImpl() native sqlfs implementation

--- jni/info_guardianproject_iocipher_File.cpp
+++ jni/info_guardianproject_iocipher_File.cpp
@@ -24,6 +24,7 @@
 #include "ScopedUtfChars.h"
 #include "readlink.h"
 #include "toStringArray.h"
+#include "sqlfs.h"
 
 #include <string>
 #include <vector>
@@ -39,6 +40,14 @@
 #include <time.h>
 #include <unistd.h>
 #include <utime.h>
+
+/* right now, we use a single global virtual file system so we don't
+ * have to map the structs sqlfs_t and sqlite3 to Java code */
+extern char databaseFileName[PATH_MAX];
+extern sqlfs_t *sqlfs;
+
+// from fuse.h
+typedef int(* 	fuse_fill_dir_t )(void *buf, const char *name, const struct stat *stbuf, off_t off);
 
 static jstring info_guardianproject_iocipher_File_readlink(JNIEnv* env, jclass, jstring javaPath) {
     ScopedUtfChars path(env, javaPath);
@@ -88,76 +97,27 @@ static jboolean info_guardianproject_iocipher_File_setLastModifiedImpl(JNIEnv* e
     return (utime(path.c_str(), &times) == 0);
 }
 
-// Iterates over the filenames in the given directory.
-class ScopedReaddir {
-public:
-    ScopedReaddir(const char* path) {
-        mDirStream = opendir(path);
-        mIsBad = (mDirStream == NULL);
-    }
-
-    ~ScopedReaddir() {
-        if (mDirStream != NULL) {
-            closedir(mDirStream);
-        }
-    }
-
-    // Returns the next filename, or NULL.
-    const char* next() {
-        dirent* result = NULL;
-        int rc = readdir_r(mDirStream, &mEntry, &result);
-        if (rc != 0) {
-            mIsBad = true;
-            return NULL;
-        }
-        return (result != NULL) ? result->d_name : NULL;
-    }
-
-    // Has an error occurred on this stream?
-    bool isBad() const {
-        return mIsBad;
-    }
-
-private:
-    DIR* mDirStream;
-    dirent mEntry;
-    bool mIsBad;
-
-    // Disallow copy and assignment.
-    ScopedReaddir(const ScopedReaddir&);
-    void operator=(const ScopedReaddir&);
-};
-
 typedef std::vector<std::string> DirEntries;
 
-// Reads the directory referred to by 'pathBytes', adding each directory entry
-// to 'entries'.
-static bool readDirectory(JNIEnv* env, jstring javaPath, DirEntries& entries) {
-    ScopedUtfChars path(env, javaPath);
-    if (path.c_str() == NULL) {
-        return false;
-    }
-
-    ScopedReaddir dir(path.c_str());
-    if (dir.isBad()) {
-        return false;
-    }
-    const char* filename;
-    while ((filename = dir.next()) != NULL) {
-        if (strcmp(filename, ".") != 0 && strcmp(filename, "..") != 0) {
-            // TODO: this hides allocation failures from us. Push directory iteration up into Java?
-            entries.push_back(filename);
-        }
-    }
-    return true;
+/* FUSE filler() function for use with the FUSE style readdir() that
+ * libsqlfs provides.  Note: this function only ever expects statp to
+ * be NULL and off to be 0.  buf is DirEntries */
+static int fill_dir(void *buf, const char *name, const struct stat *statp, off_t off) {
+    DirEntries *entries = (DirEntries*) buf;
+    if(statp != NULL)
+        LOGE("File.listImpl() fill_dir always expects statp to be NULL");
+    if(off != 0)
+        LOGE("File.listImpl() fill_dir always expects off to be 0");
+    entries->push_back(name);
+    // TODO implement returning an error (1) if something bad happened
+    return 0;
 }
 
 static jobjectArray info_guardianproject_iocipher_File_listImpl(JNIEnv* env, jclass, jstring javaPath) {
-    // Read the directory entries into an intermediate form.
+    ScopedUtfChars path(env, javaPath);
     DirEntries entries;
-    if (!readDirectory(env, javaPath, entries)) {
-        return NULL;
-    }
+    // using FUSE readdir in old getdir() style which gives us the whole thing at once
+    sqlfs_proc_readdir(sqlfs, path.c_str(), (void *)&entries, (fuse_fill_dir_t)fill_dir, 0, NULL);
     // Translate the intermediate form into a Java String[].
     return toStringArray(env, entries);
 }
