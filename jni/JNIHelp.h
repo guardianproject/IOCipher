6c40885b455db93799fbb7d5ce0661cdcd9d03c8
Hans-Christoph Steiner <hans@eds.org>
2012-05-11 17:57:41 -0400

    add LOGD() macro to complete the whole set

--- jni/JNIHelp.h
+++ jni/JNIHelp.h
@@ -167,6 +167,7 @@ inline void jniLogException(JNIEnv* env, int priority, const char* tag, jthrowab
 #define LOGE_EX(env, ...) LOG_EX(env, LOG_ERROR, LOG_TAG, ##__VA_ARGS__)
 
 #define  LOGV(...)  __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
