085e7bfe92ca1fa87590e8cae9835b1ee7668b6d
Hans-Christoph Steiner <hans@eds.org>
2013-08-12 20:45:53 -0400

    set the NDK APP_PLATFORM based on minSdkVersion in AndroidManifest.xml
    This way we avoid annoying, arcane build errors caused by different parts
    of the build being built for different platform levels.

--- jni/Application.mk
+++ jni/Application.mk
@@ -2,5 +2,7 @@ NDK_TOOLCHAIN_VERSION=4.7
 APP_PROJECT_PATH := $(shell pwd)
 APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/jni/Android.mk
 APP_STL := stlport_static
+# the NDK platform level, aka APP_PLATFORM, is equivalent to minSdkVersion
+APP_PLATFORM := android-$(shell sed -n 's,.*android:minSdkVersion="\([0-9][0-9]*\)".*,\1,p' AndroidManifest.xml)
 #
 EXTERNAL_PATH := $(APP_PROJECT_PATH)/external
