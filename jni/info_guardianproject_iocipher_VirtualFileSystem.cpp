4e4c55bdd59eaa6d8588dc57241a1c3eeb6063f3
Hans-Christoph Steiner <hans@eds.org>
2014-09-02 20:49:09 -0400

    create mount/umount logic based on holding open a sqlfs instance
    It turns out that IOCipher needs to have a mounted and unmounted state in
    order to trigger when the key/password is zeroed out. So sqlfs_open() is
    used to create the first sqlfs instance and keep it open.  The actual work
    is done by sqlfs instances that are created on the fly for each thread.
    That keeps it flexible so that a new sqlfs instance is automatically
    instantiated whenever there is an IOCipher call in a new thread.
    
    fixes #3402 https://dev.guardianproject.info/issues/3402

--- jni/info_guardianproject_iocipher_VirtualFileSystem.cpp
+++ jni/info_guardianproject_iocipher_VirtualFileSystem.cpp
@@ -12,6 +12,8 @@
 
 // yes, dbFileName is a duplicate of default_db_file in sqlfs.c
 char dbFileName[PATH_MAX] = { 0 };
+// store first sqlfs instance as marker for mounted state
+static sqlfs_t *sqlfs = NULL;
 
 void setDatabaseFileName(JNIEnv *env, jobject obj) {
     jclass cls = env->GetObjectClass(obj);
@@ -27,85 +29,92 @@ void setDatabaseFileName(JNIEnv *env, jobject obj) {
 }
 
 static void VirtualFileSystem_mount_unencrypted(JNIEnv *env, jobject obj) {
-    char buf[256];
     setDatabaseFileName(env, obj);
-    snprintf(buf, 255, "Could not mount filesystem in %s", dbFileName);
-    if (sqlfs_init(dbFileName) != 0)
-        jniThrowException(env, "java/lang/IllegalArgumentException", buf);
+    if (!sqlfs_open(dbFileName, &sqlfs)) {
+        char msg[256];
+        snprintf(msg, 255, "Could not mount filesystem in '%s'", dbFileName);
+        jniThrowException(env, "java/lang/IllegalArgumentException", msg);
+    }
 }
 
 static void VirtualFileSystem_mount(JNIEnv *env, jobject obj, jstring javaPassword) {
-    char buf[256];
-    setDatabaseFileName(env, obj);
-    snprintf(buf, 255, "Could not mount filesystem in %s, bad password given?", dbFileName);
+    char msg[256];
+    if (sqlfs) {
+        snprintf(msg, 255, "Filesystem in '%s' already mounted!", dbFileName);
+        jniThrowException(env, "java/lang/IllegalStateException", msg);
+        return;
+    }
 
-    sqlfs_t *sqlfs = 0;
+    setDatabaseFileName(env, obj);
+
     char const *password = env->GetStringUTFChars(javaPassword, 0);
     jsize passwordLen = env->GetStringUTFLength(javaPassword);
 
     /* Attempt to open the database with the password, then immediately close
      * it. If it fails, then the password is likely wrong. */
     if (!sqlfs_open_password(dbFileName, password, &sqlfs)) {
-        LOGI("sqlfs_open_password FAILED");
-        jniThrowException(env, "java/lang/IllegalArgumentException", buf);
-    }
-    sqlfs_close(sqlfs);
-
-    /* init the vfs by caching the password in memory.  Then as threads are
-     * spawned, they will use that cached password to open a connection to the
-     * database.  sqlfs_init_password returns 0 for success in the unix fashion */
-    if (sqlfs_init_password(dbFileName, password) != 0) {
-        LOGI("sqlfs_init_password FAILED");
-        jniThrowException(env, "java/lang/IllegalArgumentException", "Initializing VFS failed.");
+        snprintf(msg, 255,
+                 "Could not mount filesystem in %s, bad password given?", dbFileName);
+        jniThrowException(env, "java/lang/IllegalArgumentException", msg);
     }
     env->ReleaseStringUTFChars(javaPassword, password);
 }
 
 static void VirtualFileSystem_mount_byte(JNIEnv *env, jobject obj, jbyteArray javaKey) {
-    char buf[256];
-    sqlfs_t *sqlfs = 0;
-    jsize keyLen = env->GetArrayLength(javaKey);
+    char msg[256];
+    if (sqlfs) {
+        snprintf(msg, 255, "Filesystem in '%s' already mounted!", dbFileName);
+        jniThrowException(env, "java/lang/IllegalStateException", msg);
+        return;
+    }
 
+    jsize keyLen = env->GetArrayLength(javaKey);
     if (keyLen != REQUIRED_KEY_LENGTH) {
-        snprintf(buf, 255, "Key length is not %i bytes (%i bytes)!",
+        snprintf(msg, 255, "Key length is not %i bytes (%i bytes)!",
                  REQUIRED_KEY_LENGTH, keyLen);
-        jniThrowException(env, "java/lang/IllegalArgumentException", buf);
+        jniThrowException(env, "java/lang/IllegalArgumentException", msg);
         return;
     }
 
     setDatabaseFileName(env, obj);
     jbyte *key = env->GetByteArrayElements(javaKey, NULL); //direct mem ref
 
-    /* 
-     * attempt to open the database with the key
-     * if it fails, then the key is likely wrong
-     */
-    snprintf(buf, 255, "Could not mount filesystem in %s, bad key given?", dbFileName);
-    // TODO create sqlfs_open_key() and sqlfs_open_password()
+    /* attempt to open the database with the key if it fails, most likely the
+     * db file does not exist or the key is wrong */
     if (!sqlfs_open_key(dbFileName, (uint8_t*)key, keyLen, &sqlfs)) {
-        LOGI("sqlfs_open_key FAILED");
-        jniThrowException(env, "java/lang/IllegalArgumentException", buf);
+        snprintf(msg, 255,
+                 "Could not mount filesystem in %s, bad key given?", dbFileName);
+        jniThrowException(env, "java/lang/IllegalArgumentException", msg);
     }
-    LOGE("sqlfs_close %p", sqlfs);
-    sqlfs_close(sqlfs);
 
-    /*
-     * init the vfs by storing the key in memory
-     * sqlfs_init_key returns 0 for success in the unix fashion
-     */
-    if (sqlfs_init_key(dbFileName, (uint8_t*)key, keyLen) != 0) {
-        LOGI("sqlfs_init_key FAILED");
-        jniThrowException(env, "java/lang/IllegalArgumentException", "Initializing VFS failed.");
-    }
     env->ReleaseByteArrayElements(javaKey, key, 0);
 }
 
 static void VirtualFileSystem_unmount(JNIEnv *env, jobject) {
-    dbFileName[0] = '\0';
+    char msg[256];
+    if (!sqlfs) {
+        snprintf(msg, 255, "Filesystem in '%s' not mounted!", dbFileName);
+        jniThrowException(env, "java/lang/IllegalStateException", msg);
+        return;
+    }
+    /* VFS holds a sqlfs instance open purely as a marker of the filesystem
+     * being mounted. If there is more than one sqlfs instance, that means
+     * that threads are still active. Closing the final sqlfs instances causes
+     * libsqlfs to close the database and zero out the key/password. */
+    if (sqlfs_instance_count() > 1) {
+        snprintf(msg, 255,
+                 "Cannot unmount when threads are still active! (%i threads)",
+                 sqlfs_instance_count() - 1);
+        jniThrowException(env, "java/lang/IllegalStateException", msg);
+        return;
+    }
+    sqlfs_close(sqlfs);
+    sqlfs = NULL;
+    memset(dbFileName, 0, PATH_MAX);
 }
 
 static jboolean VirtualFileSystem_isMounted(JNIEnv *env, jobject obj) {
-    return strlen(dbFileName) > 0;
+    return sqlfs != NULL || sqlfs_instance_count() > 0;
 }
 
 static void VirtualFileSystem_beginTransaction(JNIEnv *env, jobject) {
