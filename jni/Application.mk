977a4d89e4d0ece2f05f9f38dbfc18c1ca873471
Hans-Christoph Steiner <hans@eds.org>
2019-07-04 16:58:53 +0200

    add 64-bit ABIs: arm64-v8a x86_64

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_PROJECT_PATH := $(shell pwd)
 APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/jni/Android.mk
 APP_STL := stlport_static
