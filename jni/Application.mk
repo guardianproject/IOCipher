925e3202e3a243e83cb7b5a41c169941232eb9ef
n8fr8 <nathan@guardianproject.info>
2019-09-27 16:05:28 -0400

    update build files to work with latest gradle and Java

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_PROJECT_PATH := $(shell pwd)
 APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/jni/Android.mk
-APP_STL := stlport_static
+APP_STL := c++_static
 #
 EXTERNAL_PATH := $(APP_PROJECT_PATH)/external
