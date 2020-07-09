f6728047579e708f15dbfc75f2b3d8a10f384069
n8fr8 <nathan@guardianproject.info>
2020-07-09 13:49:41 -0400

    update jni build files for 64-bit

--- jni/Android.mk
+++ jni/Android.mk
@@ -16,6 +16,14 @@ LOCAL_SRC_FILES  := ../external/libsqlfs/sqlfs.c
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libsqlcipher
+LOCAL_SRC_FILES := $(EXTERNAL_PATH)/libs/$(TARGET_ARCH_ABI)/libsqlcipher.so
+LOCAL_EXPORT_C_INCLUDES := external
+LOCAL_EXPORT_LDFLAGS := \
+	-L$(LOCAL_PATH)/../external/libs/$(TARGET_ARCH_ABI)
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE    := libiocipher
 LOCAL_STATIC_LIBRARIES := libsqlfs
 LOCAL_SHARED_LIBRARIES := libsqlcipher
@@ -46,10 +54,3 @@ LOCAL_SRC_FILES  := ../external/libsqlfs/sqlfscat.c
 include $(BUILD_EXECUTABLE)
 
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libsqlcipher
-LOCAL_SRC_FILES := $(EXTERNAL_PATH)/libs/$(TARGET_ARCH_ABI)/libsqlcipher.so
-LOCAL_EXPORT_C_INCLUDES := external
-LOCAL_EXPORT_LDFLAGS := \
-	-L$(LOCAL_PATH)/../external/libs/$(TARGET_ARCH_ABI)
-include $(PREBUILT_SHARED_LIBRARY)
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,7 @@
-APP_ABI := armeabi armeabi-v7a x86
+APP_ABI      			:= armeabi-v7a x86 arm64-v8a x86_64
+APP_PLATFORM 			:= android-16
 APP_PROJECT_PATH := $(shell pwd)
 APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/jni/Android.mk
-APP_STL := stlport_static
+APP_STL := c++_static
 #
 EXTERNAL_PATH := $(APP_PROJECT_PATH)/external
